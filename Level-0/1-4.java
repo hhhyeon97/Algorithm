ef6b32a5859dc0b48a1f3f9733eadcecca80b2b3
minsol <kth083030@naver.com>
2024-05-17 00:47:13 +0900

    test!

--- Level-0/1-4.java
+++ Level-0/1-4.java
@@ -1,14 +1,14 @@
 // 사칙연산
 
 // 두 수의 합
-class Solution {
+class Solution1 {
     public int solution(int num1, int num2) {
         int sum = num1 + num2;
         return sum;
     }
 }
 // 두 수의 차
-class Solution1 {
+class Solution2 {
     public int solution(int num1, int num2) {
         int num = num1-num2;
         return num;
--- Level-0/5-8.java
+++ Level-0/5-8.java
@@ -1,6 +1,6 @@
 // 두 수의 나눗셈
 
-class Solution {
+class Solution5 {
     public int solution(int num1, int num2) {
         // num1을 num2로 나눈 후 1,000을 곱한 값을 계산
         double result = (double) num1 / num2 * 1000;
@@ -9,3 +9,17 @@ class Solution {
         return (int) result;
     }
 }
+
+// 숫자 비교하기
+
+class Solution6 {
+    public int solution(int num1, int num2) {
+        if(num1==num2){
+            return 1;
+        }else {
+            return -1;
+        }
+    }
+}
+
+// 분수의 덧셈 (멍미...여기서부터 막힘 !)
