e28c671f845e7dfb265577359408b9bd0881fc89
minsol <kth083030@naver.com>
2024-05-18 22:50:06 +0900

    양꼬치..

--- Level-0/13-16.java
+++ Level-0/13-16.java
@@ -46,3 +46,47 @@ class Solution14_2 {
         return answer;
     }
 }
+
+
+// 배열의 평균값
+
+class Solution15_1 {
+    public double solution(int[] numbers) {
+      // 배열의 합을 계산
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        // 평균값을 계산
+        double avg = (double) sum / numbers.length;
+
+        return avg;
+    }
+}
+
+// 다른 풀이 
+
+class Solution15_2 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+        for(int i=0; i<numbers.length; i++){
+        sum += numbers[i];
+        }
+        answer = (double)sum / numbers.length;
+        return answer;
+    }
+}
+
+// 양꼬치
+
+class Solution16 {
+    public int solution(int n, int k) {
+        int basicFee = n * 12000;
+        int drinkFee = k * 2000;
+        int discount = (n / 10) * 2000;
+        
+        return basicFee + drinkFee - discount;
+    }
+}
