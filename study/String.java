9e5f254611eb0c2efeedcd79d5e9ad927e0f4889
minsol <kth083030@naver.com>
2024-06-19 04:25:17 +0900

    문자열 셔플링

--- study/String.java
+++ study/String.java
@@ -31,3 +31,32 @@ class Solution {
 }
 
 // 배열을 순회하면서 각 아이템이 주어진 규칙에 맞는지 확인하여 해당할 때 개수를 추가한다 !
+
+
+
+/*
+https://leetcode.com/problems/shuffle-string/description/
+
+1528. Shuffle String
+
+문자열을 주어진 인덱스 배열에 따라 셔플하는 과정
+배열을 이용해 각 문자를 적절한 위치에 배치하고, 
+최종적으로 결과 배열을 문자열로 변환하여 반환하는 방식으로 문제를 해결한다. 
+이 코드는 시간 복잡도가 O(n)으로, 각 문자를 한 번씩만 처리하기 때문에 효율적이게 접근할 수 있다.
+*/
+
+class Solution2 {
+    public String restoreString(String s, int[] indices) {
+        
+        // 결과를 저장할 문자 배열 생성
+        char[] result = new char[s.length()];
+
+        // indices 배열을 순회하면서 각 문자를 적절한 위치에 배치
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+
+         // 문자 배열을 문자열로 변환하여 반환
+        return new String(result);
+    }
+}
