fcc5bc0b2a93e62070699f6be0f1353507c2e5b3
minsol <kth083030@naver.com>
2024-06-24 03:08:40 +0900

    띠용

--- study/stack-queue2.java
+++ study/stack-queue2.java
@@ -8,8 +8,10 @@ https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
 
 */
 
+
 import java.util.LinkedList;
 import java.util.Queue;
+
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
          // 학생들의 큐를 만듭니다.
@@ -41,5 +43,37 @@ class Solution {
         }
 
         return queue.size(); // 남은 학생 수 반환
+    }
+}
+
+/*
+https://leetcode.com/problems/number-of-recent-calls/description/
+
+933. Number of Recent Calls
+
+RecentCounter 클래스를 구현하여 
+최근 3000밀리초(3초) 내에 발생한 요청 수를 추적하는 문제
+*/
+
+
+class RecentCounter {
+    private Queue<Integer> requests;
+
+    public RecentCounter() {
+        // 요청을 저장할 큐를 초기화
+        requests = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+         // 현재 요청 타임스탬프를 큐에 추가
+        requests.add(t);
+        
+        // 현재 타임스탬프에서 3000밀리초 이전의 요청들은 모두 제거
+        while (requests.peek() < t - 3000) {
+            requests.poll();
+        }
+        
+        // 큐의 크기는 최근 3000밀리초 내에 발생한 요청 수를 나타낸다.
+        return requests.size();
     }
 }
