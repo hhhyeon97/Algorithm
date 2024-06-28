a389f6718ae148382d7d77ed4411ec3c08c00b58
minsol <kth083030@naver.com>
2024-06-28 11:47:57 +0900

    힙

--- study/heap.java
+++ study/heap.java
@@ -1,3 +1,47 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+/*
+https://leetcode.com/problems/delete-greatest-value-in-each-row/submissions/1302504272/
+
+2500. Delete Greatest Value in Each Row
+
+
+각 행에서 최대 값을 제거하고, 제거된 값들 중 가장 큰 값을 누적하여 결과를 구하는 문제
+힙(우선순위 큐)를 사용하여 이 문제를 효율적으로 해결할 수 있다.
+*/
+class Solution1 {
+    public int deleteGreatestValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int answer = 0;
+
+        // 각 행을 내림차순으로 정렬
+        for (int i = 0; i < m; i++) {
+            Integer[] row = new Integer[n];
+            for (int j = 0; j < n; j++) {
+                row[j] = grid[i][j];
+            }
+            Arrays.sort(row, Collections.reverseOrder());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = row[j];
+            }
+        }
+
+        // 그리드가 빈 배열이 될 때까지 반복
+        for (int col = 0; col < n; col++) {
+            int maxValInStep = Integer.MIN_VALUE;
+            for (int row = 0; row < m; row++) {
+                maxValInStep = Math.max(maxValInStep, grid[row][col]);
+            }
+            answer += maxValInStep;
+        }
+
+        return answer;
+    }
+}
+
+
 
 
 /*
@@ -6,8 +50,7 @@ https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
 1337. The K Weakest Rows in a Matrix
 */
 
-import java.util.PriorityQueue;
-class Solution {
+class Solution2 {
     public int[] kWeakestRows(int[][] mat, int k) {
            // 우선순위 큐를 정의 (우선순위는 군인 수 -> 행 인덱스)
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
