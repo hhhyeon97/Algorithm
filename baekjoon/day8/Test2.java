e365d1c58906665ec747564809a13f0382afb672
minsol <kth083030@naver.com>
2024-07-26 18:00:42 +0900

    해시/힙 활용

---
+++ baekjoon/day8/Test2.java
@@ -0,0 +1,55 @@
+// https://www.acmicpc.net/problem/1417
+// 국회의원 선거
+
+// 메모리 :  14304 KB	시간 : 124 ms
+	
+package baekjoon.day8;
+
+
+/*
+다솜이가 매수해야하는 사람의 최솟값을 출력하는 프로그램을 작성
+
+첫째 줄에 후보의 수 N이 주어진다.
+둘째 줄부터 차례대로 기호 1번을 찍으려고 하는 사람의 수, 기호 2번을 찍으려고 하는 수, 
+이렇게 총 N개의 줄에 걸쳐 입력이 들어온다. N은 50보다 작거나 같은 자연수이고, 득표수는 100보다 작거나 같은 자연수이다.
+
+!! 다솜이가 1번 후보라 가정 
+
+첫째 줄에 다솜이가 매수해야 하는 사람의 최솟값을 출력한다.
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Test2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 받기
+        int N = Integer.parseInt(br.readLine()); // 후보자 수
+        int dasomVotes = Integer.parseInt(br.readLine()); // 다솜이의 득표 수
+        // 최대 힙 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 나머지 후보들의 득표 수를 읽어 PriorityQueue에 저장
+        for (int i = 1; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int count = 0; // 다솜이가 매수해야 하는 최소 인원 수
+
+        // 다솜이의 득표 수가 나머지 후보들의 득표 수보다 많아질 때까지
+        while (!pq.isEmpty() && pq.peek() >= dasomVotes) {
+            // 가장 높은 득표 수를 가진 후보의 득표 수를 꺼냄 (poll이 반환하면서 제거함!)
+            int topVotes = pq.poll(); 
+            // 다솜이의 득표 수 증가
+            dasomVotes++; 
+            // 가장 높은 득표 수를 가진 후보의 득표 수를 1 감소 (1 감소시키고 다시 집어 넣는 것 !)
+            pq.add(topVotes - 1); 
+             // 매수한 사람 수 증가
+            count++;
+        }
+        // 결과 출력
+        System.out.println(count);
+    }
+}
---
+++ baekjoon/day8/Test3.java
@@ -0,0 +1,60 @@
+//https://www.acmicpc.net/problem/31562
+// 전주 듣고 노래 맞히기
+
+// 메모리 :  25436 KB	시간 : 412 ms
+
+package baekjoon.day8;
+
+import java.io.*;
+import java.util.*;
+
+public class Test3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 첫번째 줄에서 N과 M을 공백으로 구분하여 입력 받기
+        String[] firstLine = br.readLine().split(" ");
+        int N = Integer.parseInt(firstLine[0]); // 노래의 개수
+        int M = Integer.parseInt(firstLine[1]); // 시도할 노래의 개수
+
+        // 노래 제목과 음을 저장할 해시맵 생성
+        Map<String, String> songMap = new HashMap<>();
+
+        // N개의 노래 정보를 읽어오기
+        for (int i = 0; i < N; i++) {
+            String[] songInfo = br.readLine().split(" ");
+            String title = songInfo[1];
+            // 처음 7개의 음을 문자열로 합침
+            String melody = String.join(" ", Arrays.copyOfRange(songInfo, 2, 9)); 
+            songMap.put(melody, title);
+        }
+
+        // M개의 시도 정보 처리하기 
+        for (int i = 0; i < M; i++) {
+            String[] guess = br.readLine().split(" ");
+            // 첫 3개의 음을 문자열로 합침
+            String queryMelody = String.join(" ", guess); 
+
+            // 해당 음에 대한 노래 찾기
+            List<String> matchingTitles = new ArrayList<>();
+            for (Map.Entry<String, String> entry : songMap.entrySet()) {
+                if (entry.getKey().startsWith(queryMelody)) {
+                    matchingTitles.add(entry.getValue());
+                }
+            }
+
+            // 결과를 문자열 빌더에 추가
+            if (matchingTitles.size() == 0) {
+                sb.append("!\n");
+            } else if (matchingTitles.size() == 1) {
+                sb.append(matchingTitles.get(0)).append("\n");
+            } else {
+                sb.append("?\n");
+            }
+        }
+
+        // 결과 출력
+        System.out.print(sb.toString());
+    }
+}
