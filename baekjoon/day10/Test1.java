27ebbff9826422f94b40c5fa5d6f45737e215b89
minsol <kth083030@naver.com>
2024-07-29 19:41:17 +0900

    이중for문으로 변경

--- baekjoon/day10/Test1.java
+++ baekjoon/day10/Test1.java
@@ -60,3 +60,47 @@ public class Test1 {
         }
     }
 }
+
+
+// ================ 이중 for문으로 보완하기 ! 
+
+class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 난쟁이들의 키를 담을 배열
+        int[] heights = new int[9];
+        int sum = 0;
+
+        // 배열에 입력받은 값 넣어주기 및 총합 계산
+        for (int i = 0; i < 9; i++) {
+            heights[i] = Integer.parseInt(br.readLine());
+            sum += heights[i];
+        }
+
+        // 배열 오름차순 정렬
+        Arrays.sort(heights);
+
+        // 난쟁이 아닌 두명 찾기
+        int x = 0, y = 0;
+
+        // 이중 for문을 돌면서 sum에서 2가지를 뺐을 때 100이 되는 두 수 찾기
+        outerLoop:
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                if (sum - heights[i] - heights[j] == 100) {
+                    x = heights[i];
+                    y = heights[j];
+                    break outerLoop; // 조건을 만족하는 경우 루프 종료
+                }
+            }
+        }
+
+        // 출력: x와 y를 제외한 나머지 난쟁이들의 키를 출력
+        for (int height : heights) {
+            if (height != x && height != y) {
+                System.out.println(height);
+            }
+        }
+    }
+}
