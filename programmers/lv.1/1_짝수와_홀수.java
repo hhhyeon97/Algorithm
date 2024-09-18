0770b93edf73d169e3e7ee18c850a17ffb981d76
minsol <kth083030@naver.com>
2024-09-19 02:29:15 +0900

    edit folder

---
+++ programmers/lv.1/1_짝수와_홀수.java
@@ -0,0 +1,13 @@
+
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+class Solution {
+    public String solution(int num) {
+        if(num%2==0){
+            return "Even";
+        }else{
+            return "Odd";
+        }
+    }
+}
