76b8b5e9ef537ae3dbf6e53fde4ab59b41350ad2
minsol <kth083030@naver.com>
2024-09-28 17:17:49 +0900

    문자열->정수변환

---
+++ programmers/lv.1/5_문자열을_정수로_바꾸기.java
@@ -0,0 +1,10 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+class Solution {
+    public int solution(String s) {
+        // 문자열을 정수로 변환하여 반환
+        // parseInt() 메서드가 +,-도 처리해줌
+        return Integer.parseInt(s);
+    }
+}
