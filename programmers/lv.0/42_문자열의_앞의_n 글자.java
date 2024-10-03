53f0c4737c820c6acb1f23061c4a8c17114cd82e
minsol <kth083030@naver.com>
2024-10-03 13:29:59 +0900

    문자열substring

---
+++ programmers/lv.0/42_문자열의_앞의_n 글자.java
@@ -0,0 +1,11 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181907
+
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+          // my_string의 앞에서 n글자 추출하여 반환
+        return my_string.substring(0, n);
+    }
+}
