0e1f8d601aa6b5d7ed2e4971e7b4d45b66f2f2c2
minsol <kth083030@naver.com>
2024-10-06 18:56:08 +0900

    출력\

---
+++ baekjoon/bronze/10718_We_love_kriii.java
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10718
+// 13852KB	92ms
+
+public class Main {
+    public static void main(String[] args){
+        
+        for(int i=0; i<2; i++){
+             System.out.println("강한친구 대한육군");
+        }
+    }
+}
