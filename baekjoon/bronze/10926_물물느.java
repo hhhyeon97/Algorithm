a8898bcb9d1a54ab1d68ecc8de4ea9f165de6e52
minsol <kth083030@naver.com>
2024-10-06 20:18:09 +0900

    구현출력

---
+++ baekjoon/bronze/10926_물물느.java
@@ -0,0 +1,14 @@
+
+// https://www.acmicpc.net/problem/10926
+// 17748KB	172ms
+
+import java.util.*;
+public class 10926번 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String site = sc.nextLine();
+        System.out.print(site+"??!");
+        sc.close();
+    }
+}
