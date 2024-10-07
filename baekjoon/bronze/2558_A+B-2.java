ee7d123c04606675e4dd3e2e0166190e03c661a0
minsol <kth083030@naver.com>
2024-10-08 07:15:43 +0900

    구현

---
+++ baekjoon/bronze/2558_A+B-2.java
@@ -0,0 +1,21 @@
+
+// https://www.acmicpc.net/problem/2558
+// 17708KB	172ms
+
+import java.util.*;
+public class 2558번 {
+    public static void main(String[] args){
+    
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.print(A+B);
+        
+    }
+}
+
+
+
+
