9a11787268bbbf69dc4675aeffd64350e8b9005e
minsol <kth083030@naver.com>
2024-10-10 20:20:07 +0900

    직사각형넓이구하기

---
+++ baekjoon/bronze/27323_직사각형.java
@@ -0,0 +1,16 @@
+
+// https://www.acmicpc.net/problem/27323
+// 17596KB	168ms
+
+import java.util.*;
+public class 27323번 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.print(A*B);
+        sc.close();
+    }
+}
