2d4ddb661277411d6f82703ecf8f77de55dedea9
minsol <kth083030@naver.com>
2024-11-23 21:37:40 +0900

    수정:for문줄이기

--- programmers/lv.1/18_음양_더하기.java
+++ programmers/lv.1/18_음양_더하기.java
@@ -3,15 +3,16 @@
 
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
+        
         int sum = 0;
+
         for(int i=0; i<absolutes.length; i++){
             if(signs[i]==false){
                 absolutes[i] = -absolutes[i];
             }
+             sum += absolutes[i];
         }
-        for(int i=0; i<absolutes.length; i++){
-            sum += absolutes[i];
-        }        
+        
         return sum;
     }
 }
