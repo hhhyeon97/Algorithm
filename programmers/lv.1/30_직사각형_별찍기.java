3bfa5678202226b3fd91c7941fa5952044297ed0
minsol <kth083030@naver.com>
2024-12-04 00:01:55 +0900

    주석추가

--- programmers/lv.1/30_직사각형_별찍기.java
+++ programmers/lv.1/30_직사각형_별찍기.java
@@ -14,6 +14,12 @@ class Solution {
         scanner.close();
 
         // 직사각형 출력
+        /*
+        별 출력:
+        바깥쪽 for 루프는 세로(m)를 담당.
+        안쪽 for 루프는 가로(n) 길이만큼 *를 출력.
+        가로 별을 모두 출력한 뒤, System.out.println()으로 줄바꿈.
+        */
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
