023660267639ec39dd3ee338ddd0bba275b44fa4
minsol <kth083030@naver.com>
2024-12-28 00:45:08 +0900

    주석 추가

--- programmers/lv.1/47_명예의전당1.java
+++ programmers/lv.1/47_명예의전당1.java
@@ -5,8 +5,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
+        
+        // 최소 힙 사용
         PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
+
+        // 결과 담을 동적인 리스트
         List<Integer> result = new ArrayList<>();
+
+        /* 과정
+        매일 새 점수를 명예의 전당에 추가
+        k를 초과하면 최솟값을 제거
+        현재 목록의 최솟값을 결과 리스트에 추가
+        */
         
         for (int s : score) {
             hallOfFame.add(s); // 점수를 추가
@@ -17,8 +27,7 @@ class Solution {
             
             result.add(hallOfFame.peek()); // 최하위 점수를 결과에 추가
         }
-        
-        // 리스트를 배열로 변환하여 반환
+        // 리스트를 배열로 변환하여 반환 (for문 써도 되는데 stream 사용하면 좀 더 간단히 변환 가능)
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
