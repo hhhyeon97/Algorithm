e8b54b135db76ad28354c964c43493fed235daec
minsol <kth083030@naver.com>
2025-02-25 00:53:09 +0900

    문자열-정수변환

---
+++ programmers/lv.0/62_문자열을_정수로_변환하기.java
@@ -0,0 +1,11 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+class Solution {
+    public int solution(String n_str) {
+
+        int answer = Integer.parseInt(n_str);
+        return answer;
+    
+    }
+}
