beb61911088b9a9e36c19ab452f0f740097afa2b
minsol <kth083030@naver.com>
2025-03-06 23:54:31 +0900

    문자열치환

---
+++ programmers/lv.0/70_rny_string.java
@@ -0,0 +1,9 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181863
+
+class Solution {
+    public String solution(String rny_string) {
+        String answer = rny_string.replaceAll("m","rn");
+        return answer;
+    }
+}
