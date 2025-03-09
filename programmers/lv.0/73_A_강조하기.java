9d936698150c229d7d825fa17afbf4dcd5eefad8
minsol <kth083030@naver.com>
2025-03-09 22:32:21 +0900

    문자열 변환

---
+++ programmers/lv.0/73_A_강조하기.java
@@ -0,0 +1,9 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181874
+
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toLowerCase().replace("a","A");
+        return answer;
+    }
+}
