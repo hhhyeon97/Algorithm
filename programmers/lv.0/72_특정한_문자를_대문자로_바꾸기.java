ce7d4ffede508c3ce70b65d02c06cba0fd3c3e0e
minsol <kth083030@naver.com>
2025-03-09 22:23:10 +0900

    문자열변환

---
+++ programmers/lv.0/72_특정한_문자를_대문자로_바꾸기.java
@@ -0,0 +1,9 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181873
+
+class Solution {
+    public String solution(String my_string, String alp) {
+
+        return my_string.replace(alp,alp.toUpperCase());
+    }
+}
