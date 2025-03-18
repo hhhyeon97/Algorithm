679b1b10dadb696d193c3390b6a4f7559f0b4035
hhhyeon97 <148893126+hhhyeon97@users.noreply.github.com>
2025-03-19 00:02:33 +0900

    Update 88_n번째_원소부터.java

--- programmers/lv.0/88_n번째_원소부터.java
+++ programmers/lv.0/88_n번째_원소부터.java
@@ -1,6 +1,17 @@
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/181892
 
+/*
+
+Arrays.copyOfRange() 메서드
+ㄴ num_list의 n번째(즉, n-1 인덱스)부터 마지막 원소까지 복사
+
+n - 1을 시작 인덱스로 설정하는 이유
+ㄴ 배열의 인덱스가 0부터 시작하기 때문
+
+num_list.length를 끝 인덱스로 설정하면 마지막 원소까지 포함된다.
+
+*/
 import java.util.Arrays;
 
 class Solution {
