ae27def463bafd41297874529f7af93a3390ee8a
hhhyeon97 <148893126+hhhyeon97@users.noreply.github.com>
2025-03-18 23:59:55 +0900

    Create 88_n번째_원소부터.java

---
+++ programmers/lv.0/88_n번째_원소부터.java
@@ -0,0 +1,10 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181892
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, n - 1, num_list.length);
+    }
+}
