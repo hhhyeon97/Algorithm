b9c8aea6d828e2c592ae0a65bd4a9d01a1620777
hhhyeon97 <148893126+hhhyeon97@users.noreply.github.com>
2025-03-23 00:19:21 +0900

    Create 93_문자열로_변환.java

---
+++ programmers/lv.0/93_문자열로_변환.java
@@ -0,0 +1,16 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845
+
+class Solution {
+    public String solution(int n) {
+        // 방법1. 
+        String answer = n+"";
+        return answer;
+    }
+}
+
+// 방법2. 
+// String answer = Integer.toString(n);
+
+// 방법3.
+String answer = String.valueOf(n);
