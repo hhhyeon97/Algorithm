5c46ad69c8911f18e02d6c9d613ec5d984809cd8
hhhyeon97 <148893126+hhhyeon97@users.noreply.github.com>
2025-03-22 23:29:42 +0900

    Create 91_문자열_정수의_합.java

---
+++ programmers/lv.0/91_문자열_정수의_합.java
@@ -0,0 +1,14 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181849
+
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        
+        for(int i=0; i<num_str.length(); i++){
+            answer += Integer.parseInt(num_str.substring(i, i+1));
+        }
+        
+        return answer;
+    }
+}
