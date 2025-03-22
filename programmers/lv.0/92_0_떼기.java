8a2fe8dfec5518266f5c106b40cacf016c3a814d
hhhyeon97 <148893126+hhhyeon97@users.noreply.github.com>
2025-03-22 23:36:15 +0900

    Create 92_0_떼기.java

---
+++ programmers/lv.0/92_0_떼기.java
@@ -0,0 +1,17 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181847
+
+// 문자열로 된 숫자를 정수로 변환 하면 앞에 붙은 0들이 떼짐
+// ㄴ 다시 문자열로 변경 후 반환하기
+
+class Solution {
+    public String solution(String n_str) {
+        
+        int n = Integer.parseInt(n_str);
+
+        String answer = n+"";
+        
+        
+        return answer;
+    }
+}
