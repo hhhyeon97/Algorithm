99f4458724dd8c745c509b73feffc8157333985e
minsol <kth083030@naver.com>
2025-04-10 01:01:00 +0900

    배열/인덱스

---
+++ programmers/lv.0/111_카운트_업.java
@@ -0,0 +1,16 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num-start_num+1];
+        
+        int index = 0;
+        
+        for(int i=start_num; i<=end_num; i++){
+            answer[index] = i;
+            index++;
+        }
+        return answer;
+    }
+}
