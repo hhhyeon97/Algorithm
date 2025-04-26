e8c6fa3c3cd183ba96c013946234e5361b126cb0
minsol <kth083030@naver.com>
2025-04-26 23:40:56 +0900

    문자열

---
+++ programmers/lv.0/122_부분_문자열.java
@@ -0,0 +1,14 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181842
+
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        if(str2.contains(str1)){
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
