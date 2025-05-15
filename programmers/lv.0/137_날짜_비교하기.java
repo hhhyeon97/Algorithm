f18697bd7e0e1623e223fa6b7f9d72fafac48dd7
minsol <kth083030@naver.com>
2025-05-16 00:05:56 +0900

    date

---
+++ programmers/lv.0/137_날짜_비교하기.java
@@ -0,0 +1,16 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181838
+
+import java.time.*;
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        if (dateA.isBefore(dateB)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
